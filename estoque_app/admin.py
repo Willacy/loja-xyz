b75e88b98e01aeb60f050b647318322ac811825b
Willacy Santana <202016360013@ifba.edu.br>
2025-10-26 12:40:38 -0300

    teste

--- estoque_app/admin.py
+++ estoque_app/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Categoria, Produto, Fornecedor, EntradaProduto, Mov_entrada, SaidaProduto, Mov_saida
 
 # Register your models here.
+admin.site.register(Categoria)
+admin.site.register(Produto)
+admin.site.register(Fornecedor)
+admin.site.register(EntradaProduto)
+admin.site.register(Mov_entrada)
+admin.site.register(SaidaProduto)
+admin.site.register(Mov_saida)
