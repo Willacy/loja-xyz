c8f37417bdfe60be30dd545ea26e4c1aad06a342
Willacy Santana <202016360013@ifba.edu.br>
2025-11-02 12:55:01 -0300

    Criado o login

--- users_app/urls.py
+++ users_app/urls.py
@@ -14,11 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth import views as auth_views
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('estoque_app.urls')),
-    path('users/', include('users_app.urls')),
+    path('logar/', auth_views.LoginView.as_view(template_name='logar.html'), name='logar'),
 ]
